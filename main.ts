690f5de5e621114aa15c0dcf69bd16bf344992c2
Muhammad-Nawaz453 <157411548+Muhammad-Nawaz453@users.noreply.github.com>
2024-05-30 23:33:19 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,66 @@
+
+var message = `hello world`
+
+var message = `hello karachi`
+console.log(message);
+var sould be used
+
+
+
+let message1 = `nawaz` //let can be changed.
+message1 = `ali`
+const message2 =   `muhammad`
+// message2 = `ahmed` this will not work because const can't be changed.
+console.log(message1);
+console.log(message2);
+
+DATA TYPES
+string text
+number numberss
+boolean true or false 
+
+let firstName: string = "nawaz";
+let age: number = 23;
+let isMarried: boolean = false;
+
+console.log(`My name is ${firstName}, my age is ${age} and my married statement is ${isMarried}`)
+
+
+let num1 = 1;
+let num2 = 1;
+
+console.log(num1 + num2);
+
+let num1 = 1;
+let num2 = '1';
+
+console.log(num1 + num2);
+
+let myName = "nawaz"
+let message= "Hello " + myName;
+console.log(message);
+
+let a = 10**2;
+let b = 5;
+let c = a % b;
+console.log(a);
+
+
+let year = 2028;
+if (year %4 === 0)
+{
+    console.log("this year is leap year");
+}
+else
+{
+    console.log("this year is not leap year");
+}
+install npm i inquirer: inquirer file
+npm init -y: package.json file
+
+ 
+
+
+
+
+
